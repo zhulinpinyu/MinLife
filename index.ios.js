4c58c24252c6ec9661607c2227a89da98d3d2538
zhulinpinyu <mulixiang199016@gmail.com>
2017-02-27 23:22:03 +0800

    配置eslint

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native'
 import App from './src/App'
 
-AppRegistry.registerComponent('MinLife', () => App);
+AppRegistry.registerComponent('MinLife', () => App)
