c27c6ea6ddc8c8adf3c4d799349a33aa5931bc0b
zhulinpinyu <mulixiang199016@gmail.com>
2017-02-27 23:39:44 +0800

    添加手势解锁

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from 'react'
+
+import GLogin from './components/GLogin'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <GLogin />
     )
   }
-}
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 }
---
+++ src/components/GLogin.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Alert } from 'react-native'
+import GesturePassword from 'react-native-gesture-password'
+
+export default class GLogin extends Component {
+  state = {
+    message: '请输入手势密码解锁',
+    status: 'normal',
+    interval: 0
+  }
+
+  onEnd(password) {
+    if (password === '1235789') {
+      this.setState({
+        status: 'right',
+        message: '解锁🔓成功'
+      })
+      Alert.alert('解锁🔓成功')
+    } else {
+      this.setState({
+        status: 'wrong',
+        message: '解锁🔓失败，请重试',
+        interval: 2000
+      })
+      Alert.alert('解锁🔓失败')
+    }
+  }
+
+  onStart() {
+    this.setState({
+        status: 'normal',
+        message: '请输入手势密码解锁'
+    })
+  }
+
+  onReset() {
+    //interval 设置为1.6秒 过时自动清理手势图案
+    this.setState({
+      status: 'normal',
+      message: '请输入手势密码解锁'
+    })
+  }
+
+  render() {
+    return (
+      <GesturePassword
+        ref='pg'
+        interval={this.state.interval}
+        status={this.state.status}
+        message={this.state.message}
+        onStart={this.onStart.bind(this)}
+        onEnd={this.onEnd.bind(this)}
+        onReset={this.onReset.bind(this)}
+      />
+    )
+  }
+}
