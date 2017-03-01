3ef94900b34bacef400a1861c38ae7f7c34a3d94
zhulinpinyu <mulixiang199016@gmail.com>
2017-03-01 23:18:24 +0800

    tab 切换基本框架搭建

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Alert, View, Text } from 'react-native'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>
+            +
+          </Text>
+        </View>
+        <View>
+          <Text>
+            List
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,41 +1,120 @@
 import React, { Component } from 'react'
-
+import { View, Text } from 'react-native'
 import {
   Container,
   Content,
   Footer,
   FooterTab,
   Button,
-  Icon,
-  Text
+  Icon
 } from 'native-base'
 
+import Home from './Home'
+
 export default class Main extends Component {
+  state = {
+    activeTab: 'home'
+  }
+
+  isActive(tab) {
+    return this.state.activeTab === tab
+  }
+
+  renderTabScreen() {
+    switch (this.state.activeTab) {
+      case 'home':
+        return (
+          <Home />
+        )
+      case 'account':
+        return (
+          <View>
+            <Text>
+              account
+            </Text>
+          </View>
+        )
+      case 'stats':
+        return (
+          <View>
+            <Text>
+              Stats
+            </Text>
+          </View>
+        )
+      case 'person':
+        return (
+          <View>
+            <Text>
+              Person
+            </Text>
+          </View>
+        )
+      default:
+        return null
+    }
+  }
+
   render() {
     return (
-      <Container>
-        <Content />
+      <Container style={styles.container}>
+        <Content>
+          {this.renderTabScreen()}
+        </Content>
         <Footer >
           <FooterTab>
-            <Button active>
-              <Icon name="ios-home" />
+            <Button
+              active={this.state.activeTab === 'home'}
+              style={styles.tabBtn}
+              onPress={() => this.setState({ activeTab: 'home' })}
+            >
+              <Icon
+                name={this.isActive('home') ? 'ios-home' : 'ios-home-outline'}
+              />
               <Text>主页</Text>
             </Button>
-            <Button>
-              <Icon name="ios-card-outline" />
+            <Button
+              active={this.state.activeTab === 'account'}
+              style={styles.tabBtn}
+              onPress={() => this.setState({ activeTab: 'account' })}
+            >
+              <Icon
+                name={this.isActive('account') ? 'ios-card' : 'ios-card-outline'}
+              />
               <Text>账户</Text>
             </Button>
-            <Button>
-              <Icon name="ios-stats-outline" />
+            <Button
+              active={this.state.activeTab === 'stats'}
+              style={styles.tabBtn}
+              onPress={() => this.setState({ activeTab: 'stats' })}
+            >
+              <Icon
+                name={this.isActive('stats') ? 'ios-stats' : 'ios-stats-outline'}
+              />
               <Text>图表</Text>
             </Button>
-            <Button>
-              <Icon name="person" />
+            <Button
+              active={this.state.activeTab === 'person'}
+              style={styles.tabBtn}
+              onPress={() => this.setState({ activeTab: 'person' })}
+            >
+              <Icon
+                name={this.isActive('person') ? 'ios-person' : 'ios-person-outline'}
+              />
               <Text>我</Text>
             </Button>
           </FooterTab>
         </Footer>
       </Container>
     )
+  }
+}
+
+const styles = {
+  container: {
+    paddingTop: 10
+  },
+  tabBtn: {
+    borderRadius: 0
   }
 }
