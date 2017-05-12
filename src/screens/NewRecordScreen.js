e067ab8bf5021c680870f51372ec1b181b4b4a7e
zhulinpinyu <mulixiang199016@gmail.com>
2017-05-12 17:49:12 +0800

    基本的form表单试用

--- src/screens/NewRecordScreen.js
+++ src/screens/NewRecordScreen.js
@@ -1,44 +1,74 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { ButtonGroup } from 'react-native-elements'
+import { View } from 'react-native'
+import t from 'tcomb-form-native'
+import moment from 'moment'
 
 class NewRecordScreen extends Component {
 
   state = {
-    selectedIndex: 0
-  }
-
-  updateIndex = (selectedIndex) => {
-    this.setState({ selectedIndex })
-  }
-
-  renderForms() {
-    switch (this.state.selectedIndex) {
-      case 0:
-        return <Text>支出</Text>
-      case 1:
-        return <Text>收入</Text>
-      case 2:
-        return <Text>转账</Text>
-      default:
-        return <Text>支出</Text>
-    }
+    text: null
   }
 
   render() {
-    const buttons = ['支出', '收入', '转账']
-    const { selectedIndex } = this.state
+    const Person = t.struct({
+      type: t.enums({
+        payment: '支出',
+        income: '收入',
+        transfer: '转账'
+      }),
+      money: t.Number,
+      category: t.String,
+      account: t.Number,
+      members: t.enums({
+        brj: 'LaoPo',
+        mlx: 'LaoGong',
+        home: 'Home'
+      }),
+      createdAt: t.Date,
+      notes: t.String
+    });
 
+    const options = {
+      fields: {
+        type: {
+          label: '账目类型',
+          nullOption: false
+        },
+        money: {
+          label: '金额'
+        },
+        category: {
+          label: '类别'
+        },
+        account: {
+          label: '账户'
+        },
+        members: {
+          label: '成员',
+          nullOption: false
+        },
+        createdAt: {
+          label: '日期',
+          mode: 'datetime',
+          config: {
+            format: (date) => String(moment(date).format('lll'))
+          }
+        },
+        notes: {
+          label: '备注'
+        }
+      }
+    }
+
+    const Form = t.form.Form
     return (
-      <View style={{ flex: 1, backgroundColor: '#eeeeee' }}>
-        <ButtonGroup
-          onPress={this.updateIndex}
-          selectedIndex={selectedIndex}
-          selectedBackgroundColor='gray'
-          buttons={buttons}
-          containerStyle={{ height: 50 }}
+      <View style={{ margin: 10, flex: 1 }} >
+        <Form
+          ref="form"
+          type={Person}
+          options={options}
+          value={{ type: 'payment', members: 'brj' }}
         />
-        {this.renderForms()}
       </View>
     )
   }
